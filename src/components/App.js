e70350ac9d11e0a3b24c377fb3a7ce18185c6111
KiSung <kisung.dev@gmail.com>
2019-01-14 09:55:34 +0900

    applying prettier eslint

--- src/components/App.js
+++ src/components/App.js
@@ -3,72 +3,81 @@ import PageTemplate from './PageTemplate';
 import TodoInput from './TodoInput';
 import TodoList from './TodoList';
 
+const a = () => '2';
 const initialTodos = new Array(500)
   .fill(0)
   .map((foo, index) => ({ id: index, text: `일정${index}`, done: false }));
 class App extends Component {
   state = {
     input: '',
-    todos: initialTodos
+    todos: initialTodos,
     // [
     //   { id: 0, text: '리액트 공부', done: true },
     //   { id: 1, text: '컴포넌트 스타일링 해보기', done: false }
     // ]
   };
+
   handleChange = e => {
     const { value } = e.target;
     this.setState({
-      input: value
+      input: value,
     });
   };
 
   id = 1;
-  getId = () => {
-    return ++this.id;
-  };
+
+  getId = () => ++this.id;
+
   handleChange = e => {
     const { value } = e.target;
     this.setState({
-      input: value
+      input: value,
     });
   };
+
   handleInsert = () => {
     const { todos, input } = this.state;
     const newTodo = {
       text: input,
       done: false,
-      id: this.getId()
+      id: this.getId(),
     };
     this.setState({
       todos: [...todos, newTodo],
-      input: ''
+      input: '',
     });
   };
+
   handleToggle = id => {
     const { todos } = this.state;
     const index = todos.findIndex(todo => todo.id === id);
 
     const toggled = {
       ...todos[index],
-      done: !todos[index].done
+      done: !todos[index].done,
     };
 
     this.setState({
       todos: [
         ...todos.slice(0, index),
         toggled,
-        ...todos.slice(index + 1, todos.length)
-      ]
+        ...todos.slice(index + 1, todos.length),
+      ],
     });
   };
+
   handleRemove = id => {
     const { todos } = this.state;
     const index = todos.findIndex(todo => todo.id === id);
 
     this.setState({
-      todos: [...todos.slice(0, index), ...todos.slice(index + 1, todos.length)]
+      todos: [
+        ...todos.slice(0, index),
+        ...todos.slice(index + 1, todos.length),
+      ],
     });
   };
+
   render() {
     const { input, todos } = this.state;
     const { handleChange, handleToggle, handleInsert, handleRemove } = this;
